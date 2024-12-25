0811f033c553d3e4037d9689f5cffe31e47974bd
Catherine Lu <cathyhulu@gmail.com>
2024-12-25 16:11:43 -0600

    convert csv to json python script

---
+++ src/_scripts/csv-json.py
@@ -0,0 +1,26 @@
+import csv
+import json
+
+def make_json (csvFilePath, jsonFilePath):
+  jsonData = {}
+  jsonData['data'] = []
+
+  with open(csvFilePath, mode='r') as file:
+    csvFile = csv.DictReader(file)
+    for rows in csvFile:
+      rowData = {}
+      for key, value in rows.items():
+        if value.isnumeric():
+          value =int(value)
+          rowData[key] = value
+      jsonData['data'].append(rowData)
+  print(jsonData)
+
+  with open(jsonFilePath, mode='w') as jsonFile:
+    jsonFile.write(json.dumps(jsonData, indent=4))
+
+
+csvFilePath = '../_data/test.csv'
+jsonFilePath = '../_data/test.json'
+
+make_json(csvFilePath, jsonFilePath)
